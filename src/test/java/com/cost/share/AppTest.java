476eee1ec943553b645d7445d7c5f7e965b04eaf
vineetsahu87 <vineetsahu87@gmail.com>
2019-06-15 12:25:37 +0530

    Base Project Structure Checkin
    Checking in the multi module project

---
+++ src/test/java/com/cost/share/AppTest.java
@@ -0,0 +1,20 @@
+package com.cost.share;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
